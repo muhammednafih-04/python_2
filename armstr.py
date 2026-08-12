707e8a6e3271eb543a619d8aad0c4039a57644b1
hasna1819 <nafihnisar05@gmail.com>
2026-08-12 12:26:56 +0530

    second commit

---
+++ armstr.py
@@ -0,0 +1,12 @@
+num=int(input("enter a numbrer:"))
+a=num
+sum=0
+total=len(str(a))
+while num>0:
+    digit=num%10
+    num=num//10
+    sum+=digit**total
+if sum==a:
+    print("it is an armstrong number")
+else:
+     print("it is not an armstrong number")
